5c296405d7b044f244bff31acda7a0bf155da0ec
Trenton Foster <trentoneio@gmail.com>
2023-09-25 16:01:55 -0500

    Update upload script
    Updated the script that uploads a hex file from the RPi to the arduino
    so that the user is requried to put the loction of the .hex file to be
    uploaded on the same line as the script call.
    Later, this will allow for multiple arduino projects to be used in one
    large project.
    
    Author: Trenton Foster
    Issue: Code Improvement

--- upload_to_arduino.sh
+++ upload_to_arduino.sh
@@ -3,8 +3,7 @@
 # This code provides, frankly, a pretty terrible, but easy wrapper around avrdude.
 # Using this script you can upload hex files from a RPi to a arduino. All you need to do is:
 # 1. Locate the .hex file to upload (https://www.aranacorp.com/en/generating-and-uploading-hex-files-to-an-arduino/)
-# 2. Enter this the location of the .hex file when prompted.
+# 2. Enter this the location of the on the CLI directly behind the script name when calling this script
 
-read -p "Enter .hex file location (file to upload): " UPLOAD_LOC
-
-/home/pi/Downloads/arduino-1.8.19/hardware/tools/avr/bin/avrdude -C/home/pi/Downloads/arduino-1.8.19/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:$UPLOAD_LOC:i 
+UPLOAD_LOC=$1
+/home/pi/Downloads/arduino-1.8.19/hardware/tools/avr/bin/avrdude -C/home/pi/Downloads/arduino-1.8.19/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:$UPLOAD_LOC:i
